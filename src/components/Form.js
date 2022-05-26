1aea3c0e3f176ed6598f8600e9cc90235ce0d751
CAUAM RIBEIRO MARQUE <cauam.rmarque@senacsp.edu.br>
2022-05-26 11:30:44 -0300

    projeto cadastro de usuario concluido front

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,26 @@
 import {useState} from "react"
+import {path} from "../config/path"
+
+let n = "";
+let e = "";
+let c = "";
+let t = "";
+let i = "";
+let u = "";
+let s = "";
+
 export default function Form(){
     const [nome,setNome]=useState("");
-    const [nome,setEmail]=useState("");
-    const [nome,setCPF]=useState("");
-    const [nome,setTelefone]=useState("");
-    const [nome,setIdade]=useState("");
-    const [nome,setUsuario]=useState("");
-    const [nome,setSenha]=useState("");
+    const [email,setEmail]=useState("");
+    const [cpf,setCPF]=useState("");
+    const [telefone,setTelefone]=useState("");
+    const [idade,setIdade]=useState("");
+    const [usuario,setUsuario]=useState("");
+    const [senha,setSenha]=useState("");
+
+
+
+
     return(
         <div className="form">
             <h2>Cadastro de Cliente</h2>
@@ -16,42 +30,75 @@ export default function Form(){
 
 
             <div class="mb-3">
-            <label for="NomeCompleto" class="form-label">Nome Completo</label>
-            <input type="text" class="form-control" id="NomeCompleto" placeholder="Nome do Cliente"/>
+            <label htmlFor="NomeCompleto" className="form-label">Nome Completo</label>
+            <input type="text" 
+                class="form-control" 
+                id="NomeCompleto" 
+                placeholder="Nome do Cliente"
+                value={nome}
+                onChange={(value)=>setNome(value.target.value)}
+                />
         </div>
         <div class="mb-3">
-            <label for="Email" class="form-label">E-Mail</label>
-            <input type="email" class="form-control" id="Email" placeholder="Email"/>
+            <label htmlFor="Email" className="form-label">E-Mail</label>
+            <input type="email" className="form-control" id="Email" placeholder="Email"
+            value={email}
+            onChange={(value)=>setEmail(value.target.value)}
+            />
         </div>
 
-        <div class="mb-3">
-            <label for="CPF" class="form-label">CPF</label>
-            <input type="email" class="form-control" id="CPF" placeholder="CPF"/>
+        <div className="mb-3">
+            <label htmlFor="CPF" className="form-label">CPF</label>
+            <input type="text" className="form-control" id="CPF" placeholder="CPF"
+            value={cpf}
+            onChange={(value)=>setCPF(value.target.value)}
+            />
         </div>
 
-        <div class="mb-3">
-            <label for="Telefone" class="form-label">Telefone</label>
-            <input type="tel" class="form-control" id="Telefone" placeholder="Telefone"/>
+        <div className="mb-3">
+            <label htmlFor="Telefone" className="form-label">Telefone</label>
+            <input type="tel" className="form-control" id="Telefone" placeholder="Telefone"
+            value={telefone}
+            onChange={(value)=>setTelefone(value.target.value)}
+            />
         </div>
 
-        <div class="mb-3">
-            <label for="Idade" class="form-label">Idade</label>
-            <input type="Number" class="form-control" id="Idade" placeholder="Idade"/>
+        <div className="mb-3">
+            <label htmlFor="Idade" className="form-label">Idade</label>
+            <input type="Number" className="form-control" id="Idade" placeholder="Idade"
+            value={idade}
+            onChange={(value)=>setIdade(value.target.value)}
+            />
         </div>
 
-        <div class="mb-3">
-            <label for="Usuario" class="form-label">Usuário</label>
-            <input type="text" class="form-control" id="Usuario" placeholder="Usuario"/>
+        <div className="mb-3">
+            <label htmlFor="Usuario" className="form-label">Usuário</label>
+            <input type="text" className="form-control" id="Usuario" placeholder="Usuario"
+            value={usuario}
+            onChange={(value)=>setUsuario(value.target.value)}
+            />
         </div>
 
-        <div class="mb-3">
-            <label for="Senha" class="form-label">E-Mail</label>
-            <input type="password" class="form-control" id="Senha" placeholder="Digite Aqui"/>
-
-            <button type="button" class="btn btn-dark">Cadastrar</button>
+        <div className="mb-3">
+            <label htmlFor="Senha" className="form-label">Senha</label>
+            <input type="password" className="form-control" id="Senha" placeholder="Digite Aqui"
+            value={senha}
+            onChange={(value)=>setSenha(value.target.value)}
+            />
+        
+            
         </div>
 
-
+        <button onClick={()=>{
+            n = nome;
+            e = email;
+            c = cpf;
+            t = telefone;
+            i = idade;
+            u = usuario;
+            s = senha;
+            gravarDados();
+        }} type="button" className="btn btn-dark">Cadastrar</button>
 
 
 
@@ -61,4 +108,35 @@ export default function Form(){
 
         </div>
     )
+}
+
+function gravarDados(){
+
+
+    fetch(`${path}/cadastro`,{
+        method:"POST",
+        headers: {
+            accept:"application/json",
+            "content-type": "application/json"
+        },
+        body:JSON.stringify({
+            nome:n,
+            email:e,
+            cpf:c,
+            telefone:t,
+            idade:i,
+            usuario:u,
+            senha:s,
+        })   
+
+
+    })
+    .then((result)=>result.json())
+    .then((data)=>{
+        alert(data.output);
+    })
+    .catch((erro)=>console.error(`Erro ao carregar a API ->${erro}`))
+
+
+    
 }
